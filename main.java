083c7ddbac7d9f455f7a5953743b2081db02ac59
Chris Scheers <chris.scheers@siemens.com>
2021-12-25 18:29:15 +0100

    Day 7

--- main.java
+++ main.java
@@ -9,7 +9,7 @@ public class main {
 //        Day4.go();
 //        Day5.go();
 //        Day6.go();
-        Day7.go();
+//        Day7.go();
 //        Day8.go();
 //        Day9.go();
 //        Day10.go();
@@ -26,7 +26,7 @@ public class main {
 //        Day21.go();
 //        Day22.go();
 //        Day23.go();
-///        Day24.go();
+        Day24.go();
 ///        Day25.go();
     }
 }
